c9aca755dc2c66bc4079b867a2b6887534a6b559
Brett Zamir <brettz9@yahoo.com>
2021-01-19 11:01:32 +0800

    - npm: Reorder scripts so main scripts at bottom; switch `test` to check coverage and add `jest` script for non-coverage testing - npm: Add ncurc file to prevent npm-check-updates auto-updating peerDeps. - npm: Self-reference current version of sonarjs - npm: Update devDeps (minus types/estree as ESLint doesn't yet support that version)

---
+++ .ncurc.js
@@ -0,0 +1,27 @@
+/*
+ * eslint-plugin-sonarjs
+ * Copyright (C) 2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+"use strict";
+
+module.exports = {
+  // Whitelist all for `npm-check-updates` checking besides `peer` which
+  //   indicates somewhat older versions of `eslint` we still support even
+  //   while our devDeps point to a more recent version
+  dep: "prod,dev,optional,bundle",
+};
